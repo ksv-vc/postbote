59fa3b55bd685a55ee3969b85c80fd6944783eb5
Nick Schmidt <nick@fintory.com>
2022-09-29 23:41:09 +0200

    chore: remove `.docusaurus` folder to gitignore

--- packages/docs/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/nick/dev/postbote/node_modules/.pnpm/infima@0.2.0-alpha.42/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/nick/dev/postbote/node_modules/.pnpm/@docusaurus+theme-classic@2.1.0_3tgeifm2vmwrlpqlopppsnjtcu/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/nick/dev/postbote/node_modules/.pnpm/@docusaurus+theme-classic@2.1.0_3tgeifm2vmwrlpqlopppsnjtcu/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/nick/dev/postbote/packages/docs/src/css/custom.css"),
-];
